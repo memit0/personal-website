43360a63f472fe54636557cbe442338afb8b1578
memit0 <mebattll@gmail.com>
2024-09-28 09:07:13 -0400

    added the about me page

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,13 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Mehmet Battal's Commonplace
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm a math & cs major @ boston university. I'm fascinated by a lot of different topics, such as 
+         fashion, programming, art, design, sports, longevity, etc. I would say I'm a jack of all trades
+         master of none, which I don't believe is necessarily a bad thing. I have a great interest in creating
+         anything that would bring significant impact and value to problems. I aim to create more than I consume.`}
       </p>
       <div className="my-8">
         <BlogPosts />
